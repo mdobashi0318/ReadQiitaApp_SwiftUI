a1d0e71950d6357ab81853b96612c01afbf6e26c
MasaharuDobashi <dobashi.masaharu@gmail.com>
2024-03-19 22:42:04 +0900

    searchableの場所を変更

--- ReadQiitaApp_SwiftUI/Screens/ArticleList/ArticleListScreen.swift
+++ ReadQiitaApp_SwiftUI/Screens/ArticleList/ArticleListScreen.swift
@@ -31,6 +31,15 @@ struct ArticleListScreen: View {
                         })
                     })
                 }
+                .searchable(text: $viewModel.searchText)
+                .onSubmit(of: .search) {
+                    viewModel.fetchArticleList()
+                }
+                .onChange(of: viewModel.searchText) { text in
+                    if text.isEmpty {
+                        viewModel.fetchArticleList()
+                    }
+                }
         }
         .onAppear {
             viewModel.fetchArticleList()
@@ -57,15 +66,6 @@ struct ArticleListScreen: View {
                 }
             }
             .listStyle(.inset)
-            .searchable(text: $viewModel.searchText)
-            .onSubmit(of: .search) {
-                viewModel.fetchArticleList()
-            }
-            .onChange(of: viewModel.searchText) { text in
-                if text.isEmpty {
-                    viewModel.fetchArticleList()
-                }
-            }
         }
     }
     
