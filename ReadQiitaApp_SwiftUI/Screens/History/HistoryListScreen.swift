5cb454638ca677aff354fb52acc961c0240bf15c
MasaharuDobashi <dobashi.masaharu@gmail.com>
2025-06-09 08:37:39 +0900

    閲覧履歴の削除機能追加

--- ReadQiitaApp_SwiftUI/Screens/History/HistoryListScreen.swift
+++ ReadQiitaApp_SwiftUI/Screens/History/HistoryListScreen.swift
@@ -15,33 +15,87 @@ struct HistoryListScreen: View {
     
     @Environment(\.dismiss) private var dismiss
     
+    @Environment(\.modelContext) private var modelContext
+    
+    @State private var confirmDelete = false
+    
     init() {
         _historyList = Query(sort: [SortDescriptor(\History.update_at, order: .reverse)])
     }
     
     var body: some View {
         NavigationStack {
+            list
+                .navigationTitle(R.string.label.history())
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button(action: {
+                            dismiss()
+                        }, label: {
+                            Image(systemName: "xmark")
+                        })
+                    }
+                    
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button(action: {
+                            confirmDelete.toggle()
+                        }, label: {
+                            Image(systemName: "trash")
+                        })
+                    }
+                }
+                .alert("全件削除しますか?", isPresented: $confirmDelete) {
+                    Button(role: .destructive, action: {
+                        allDelete()
+                    }, label: {
+                        Text(R.string.button.delete())
+                    })
+                    
+                    Button(role: .cancel, action: {
+                        confirmDelete = false
+                    }, label: {
+                        Text(R.string.button.cancel())
+                    })
+                }
+                .navigationDestination(for: History.self, destination: { history in
+                    ArticleScreen(id: history.id, url: history.url, title: history.title)
+                })
+        }
+    }
+    
+    @ViewBuilder
+    private var list: some View {
+        if historyList.isEmpty {
+            Text(R.string.label.noHistory())
+        } else {
             List {
                 ForEach(historyList) { history in
                     NavigationLink(value: history , label: {
                         Text(history.title)
                     })
                 }
+                .onDelete(perform: delete)
             }
-            .navigationTitle(R.string.label.history())
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button(action: {
-                        dismiss()
-                    }, label: {
-                        Image(systemName: "xmark")
-                    })
-                }
-            }
-            .navigationDestination(for: History.self, destination: { history in
-                ArticleScreen(id: history.id, url: history.url, title: history.title)
-            })
         }
-        
+    }
+    
+    private func delete(at offsets: IndexSet) {
+        for offset in offsets {
+            do {
+                let history = historyList[offset]
+                modelContext.delete(history)
+                try modelContext.save()
+            } catch {
+                confirmDelete = true
+            }
+        }
+    }
+    
+    private func allDelete() {
+        do {
+            try modelContext.delete(model: History.self)
+        } catch {
+            print("Historyの全削除失敗")
+        }
     }
 }
